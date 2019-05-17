cae5bd9a24942e174943c41561cd9bfdf750fae5
nilesh9985 <46082391+nilesh9985@users.noreply.github.com>
2019-05-17 16:10:03 +0530

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -3,3 +3,11 @@ node default{
 node 'waldevrddonp01.dev.rocketsoftware.com' {
   include role::master_server
 }
+
+node /^web/ {
+include role::app_server
+}
+
+node /^db/ {
+include role::db_server
+}
